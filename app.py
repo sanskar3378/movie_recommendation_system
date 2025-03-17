9df3b3aaa48f9276fd90f8bba82f61794d45edd8
sanskar <sanskarthengare.college@gmail.com>
2025-03-18 00:53:16 +0530

    updated commit

--- app.py
+++ app.py
@@ -1,30 +1,55 @@
 import streamlit as st
 import pickle
 import pandas as pd
+import requests
 
-def recommend(name):
-    movie_index = movies_df[movies_df['title'] == name].index[0]
+
+def fetch_poster(movie_id):
+    response = requests.get('https://api.themoviedb.org/3/movie/{}?api_key=7b265889effd91ac7d8e331e33932630&language=en-US'.format(movie_id))
+    data = response.json()
+    return "https://image.tmdb.org/t/p/w500/" + data['poster_path']
+
+
+def recommend(movie):
+    movie_index = movies[movies['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movie_list = sorted(list(enumerate(distances)), reverse=True, key = lambda x : x[1])[1:11]
-    ret = list()
-    for i in movie_list:
-        ret.append(movies_df.iloc[i[0]].title)
-    return ret
+    movies_list = sorted(list(enumerate(distances)), reverse=True, key = lambda x : x[1])[1:11]
+
+    recommended_movies = []
+    recommended_movies_poster = []
+
+    for i in movies_list:
+        movie_id = movies.iloc[i[0]].movie_id
+        recommended_movies.append(movies.iloc[i[0]].title)
+        # fetch poster from API
+        recommended_movies_poster.append(fetch_poster(movie_id))
+    return recommended_movies, recommended_movies_poster
 
 
-st.title("Movie Recommendation System")
-movie_dict = pickle.load(open('movies.pkl', 'rb'))
+
+movies_list = pickle.load(open('movies.pkl', 'rb'))
+movies = pd.DataFrame(movies_list)
 
 similarity = pickle.load(open('similarity.pkl', 'rb'))
 
-movies_df = pd.DataFrame(movie_dict)
+st.title("Movie Recommendation System")
 
-selected_movie_name = st.selectbox(label='Enter a movie name',
-                       options = movies_df['title'].values)
 
-# movie_name = st.text_input(label='Enter a movie name')
+selected_movie_name  = st.selectbox(label = "Enter a movie name", 
+                       options = movies['title'].values)
+
+number = st.number_input("Insert a number", format="%i", step=1)
 
 if st.button('Recommend'):
-    res = recommend(selected_movie_name)
-    for i in res:
-        st.write(i)
+    names, posters =  recommend(selected_movie_name)
+    
+    cols = st.columns(3)
+
+    for i in range(number):
+        cols[i%3].write(names[i])
+        cols[i%3].image(posters[i], width= 200)
+
+
+
+
+
